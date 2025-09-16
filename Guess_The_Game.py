0588afed4947f1b1d25f3be09d9f198e240c0a1f
Ritika katta <ritikakatta24@gmail.com>
2025-09-16 22:30:46 +0530

    added the conditions and loop

--- Guess_The_Game.py
+++ Guess_The_Game.py
@@ -3,4 +3,18 @@ print("Hello!!")
 print("Welcome To Guess the Number Game!!")
 chances=7
 cmp=random.randrange(1,101)
-Guess=int(input("enter the number"))
+print(cmp)
+while(chances>0):
+ Guess=int(input("enter the number"))
+ if(Guess==cmp):
+    print("Correct! User Wins")
+    break
+ else:
+   if(Guess>cmp):
+     chances=chances-1
+     print("Too High! Attempt left:",chances)  
+   else:
+       chances=chances-1
+       print("Too Low! Attempt left:",chances)   
+if(chances==0):
+  print("User Lost! The Number was",cmp)       
